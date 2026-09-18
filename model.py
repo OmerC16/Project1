c77f207cce1ed3927d1a5cd5a36de2bfa544c719
OmerC16 <cohen.omer2009@gmail.com>
2026-09-18 10:33:42 +0300

    Update model.py

--- model.py
+++ model.py
@@ -41,7 +41,7 @@ model_loss = np.mean(np.abs(y - y_hat))
 # Actual prediction
 st.title("Predict the downforce produced by a F1 car by it's speed")
 st.badge("Note that the prediction is relevant to when the wing is at an angle of 5 - 10 degrees (So the car is moving at a straight line)", color="green")
-number = st.number_input("Enter a speed (200 - 300) kmh", value=100, placeholder="Enter speed")
+number = st.number_input("Enter a speed (200 - 300) kmh", value=200, placeholder="Enter speed")
 if (number >= 200 and number <= 300):
   prediction = round(model.predict(np.array([[number]]))[0], 2)
   st.write("The downforce (N) that will be produced when moving at a speed of - " + int(number) + "kmh is " + float(prediction) + "N")
