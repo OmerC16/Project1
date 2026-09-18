e26933b394d13bfd15e55591aa0b9f0b5d1b7d56
OmerC16 <cohen.omer2009@gmail.com>
2026-09-18 11:11:52 +0300

    Update info.py

--- info.py
+++ info.py
@@ -16,4 +16,5 @@ st.title("Model explanation")
 
 if (st.button("Download the Dataset")):
   with st.spinner("Downloading F1 aerodynamics dataset", show_time=True):
-    path = kagglehub.dataset_download(KAGGLE_DATASET)
+    #path = kagglehub.dataset_download(KAGGLE_DATASET)
+    time.sleep(5)
