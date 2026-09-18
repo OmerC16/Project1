f6ce8ed232c5922b245f0f30f6f63dc587499a4d
OmerC16 <cohen.omer2009@gmail.com>
2026-09-18 11:11:13 +0300

    Update info.py

--- info.py
+++ info.py
@@ -5,8 +5,6 @@ import os
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-path = kagglehub.dataset_download(KAGGLE_DATASET)
-
 # Constants
 KAGGLE_DATASET = "igormerlinicomposer/f1-aerodynamic-stability-and-porpoising-150k-samp"
 FEATURE_COLUMN = "speed_kmh"
@@ -18,4 +16,4 @@ st.title("Model explanation")
 
 if (st.button("Download the Dataset")):
   with st.spinner("Downloading F1 aerodynamics dataset", show_time=True):
-    path = kagglehub.dataset_download("igormerlinicomposer/f1-aerodynamic-stability-and-porpoising-150k-samp")
+    path = kagglehub.dataset_download(KAGGLE_DATASET)
